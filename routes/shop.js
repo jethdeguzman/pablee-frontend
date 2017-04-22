1776380b1443a3c701ae57bef66123def149ed14
Jethro De Guzman <jethro@acudeen.com>
2017-04-23 02:50:21 +0800

    Initial shop page
    Signed-off-by: Jethro De Guzman <jethro@acudeen.com>

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(request, response) {
-  return response.render('shop');
+  console.log(request.user);
+  return response.render('shop', {user: request.user});
 });
 
 module.exports = router;
