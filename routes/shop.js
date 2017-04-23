f1cc12df897dd97e76e6b04fec16b48df4687a50
Jethro De Guzman <jethro@acudeen.com>
2017-04-23 10:33:39 +0800

    Display products on shop page
    Signed-off-by: Jethro De Guzman <jethro@acudeen.com>

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('./../models/product');
 
 router.get('/', function(request, response) {
-  console.log(request.user);
-  return response.render('shop', {user: request.user});
+  request.user = request.session.facebook
+  var query = {title: new RegExp(request.query.q, 'i')};
+  if (request.query.cn) {
+    query['merchant.shopify.shop.country'] = request.query.cn;
+  }
+
+  Product.find(query).populate('merchant').then(function(products) {
+    console.log(products);
+    console.log(request.query.q);
+    return response.render('shop', {user: request.user, q: request.query.q, products: products});  
+  });
+  
 });
 
 module.exports = router;
