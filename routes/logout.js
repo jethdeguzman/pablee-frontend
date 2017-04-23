c92b5b8f0d2689636625cf93e7a646f6d2e7b213
Jethro De Guzman <jethro@acudeen.com>
2017-04-23 10:33:14 +0800

    Destroy session on logout
    Signed-off-by: Jethro De Guzman <jethro@acudeen.com>

--- routes/logout.js
+++ routes/logout.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(request, response) {
+  request.session.destroy();
   request.logout();
   return response.redirect('/login');
 });
